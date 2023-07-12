014a83247ae6fbdd5740ca7d69f10586ddc5837a
wandering24 <t.gaysin@itcodegroup.ru>
2023-07-12 16:41:50 +0500

    #3 Добавил админку

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from core import models
+
+
+@admin.register(models.Customer)
+class Customer(admin.ModelAdmin):
+    list_display = ('username',)
+    search_fields = ('username',)
+    search_help_text = 'Поиску по логину покупателя'
+
+
+@admin.register(models.Item)
+class Item(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    search_help_text = 'Поиску по названию товара'
+
+
+@admin.register(models.Deal)
+class Deal(admin.ModelAdmin):
+    list_display = (
+        'customer',
+        'item',
+        'total',
+        'quantity',
+        'date',
+    )
+    search_fields = ('customer', 'item',)
+    search_help_text = 'Поиску по логину покупателя или названию товара'
