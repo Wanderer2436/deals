3cae3040bbf77d769ceeaf98727cb4a9ff14ccf1
wandering24 <t.gaysin@itcodegroup.ru>
2023-07-12 15:59:08 +0500

    #1 Добавил модели

--- core/models.py
+++ core/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    username = models.CharField('Логин покупателя', max_length=20, unique=True)
+
+    class Meta:
+        verbose_name = 'Покупатель'
+        verbose_name_plural = 'Покупатели'
+        ordering = ('username',)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class Item(models.Model):
+    name = models.CharField('Название', max_length=20, unique=True)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('name',)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Deal(models.Model):
+    customer = models.ForeignKey(
+        Customer,
+        verbose_name='Покупатель',
+        on_delete=models.CASCADE,
+        related_name='deals',
+    )
+    item = models.ForeignKey(
+        Item,
+        verbose_name='Товар',
+        on_delete=models.CASCADE,
+        related_name='deals',
+    )
+    total = models.PositiveIntegerField('Сумма сделки')
+    quantity = models.PositiveIntegerField(
+        'Количество',
+        help_text='Количество товара, в штуках',
+    )
+    date = models.DateTimeField('Дата сделки')
+
+    class Meta:
+        verbose_name = 'Сделка'
+        verbose_name_plural = 'Сделки'
+        ordering = ('-date',)
+
+    def __str__(self) -> str:
+        return f'{self.customer.username}-{self.item.name}'
