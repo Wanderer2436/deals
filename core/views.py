674d069593b64f4268dedab34811fd069392096b
wandering24 <t.gaysin@itcodegroup.ru>
2023-07-13 02:10:06 +0500

    #4 добавил вьюхи

--- core/views.py
+++ core/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.db.models import Sum
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import views
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from core import models, serializers, utils
+
+
+class CustomerAPIView(views.APIView):
+    serializer_class = serializers.Customer
+    queryset = models.Customer.objects.prefetch_related('deals').annotate(
+        spent_money=Sum('deals__total')).order_by('-spent_money')[:5]
+    permission_classes = (AllowAny,)
+
+    def get(self, request: Request) -> Response:
+        common_gems = utils.get_common_gems(queryset=self.queryset)
+        data = self.serializer_class(
+            self.queryset.all(), many=True, context={'common_gems': common_gems}
+        ).data
+        return Response({'response': data})
+
+
+class DealFileUploadAPIView(views.APIView):
+    serializer_class = serializers.DealFile
+    parser_classes = (MultiPartParser,)
+    permission_classes = (AllowAny,)
+
+    @swagger_auto_schema(request_body=serializer_class)
+    def post(self, request: Request) -> Response:
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        utils.parse_csv(file=serializer.validated_data['deals'])
+        return Response({'Status': 'OK'})
