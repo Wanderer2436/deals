851a1bf8bedea190c04ecb92338f673134f41649
wandering24 <t.gaysin@itcodegroup.ru>
2023-07-12 15:32:04 +0500

    #2 Добавил каркас проекта

---
+++ project/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import RedirectView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='SIBDEV_DEALS',
+        default_version='v1',
+    ),
+    permission_classes=[permissions.AllowAny])
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='/swagger/')),
+    path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('core/', include('core.urls', namespace='core')),
+]
